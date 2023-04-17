58ce9129c2ccc605d5f4b88718206b16103d46df
advi-bv <advibv@gmail.com>
2023-04-17 19:49:38 +0530

    final

--- GoogleSearch_test.js
+++ GoogleSearch_test.js
@@ -8,8 +8,10 @@ Scenario('test something',  ( { I } ) => {
 });
 
 Scenario('test something-2',  ( { I } ) => {
-    I.amOnPage('/');
-    I.wait(3);
-    I.see('About');
+    I.amOnPage('http://jnnce.ac.in/jnndemo/');
+    I.wait('3');
+    I.see('WELCOME TO JNNCE, SHIVAMOGGA')
+    I.click("Click here")
+    I.wait('10');
     I.saveScreenshot('b.png')
 });
